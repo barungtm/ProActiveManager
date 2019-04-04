a1ceb99877cf64f4dc1dbb0b1ecef727a531c856
proactivedevelopers <info@proactivedevelopers.com>
2019-04-04 19:06:21 +0545

    Axios

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-import { View, Text} from 'react-native';
+import { View, Text } from 'react-native';
+import axios from 'axios';
 
 class Projects extends Component {
-    componentWillMound() {
+    state = { users: [] };
 
+    componentWillMount() {
+        axios.get('http://msoftech.net/accounts/api/users')
+            .then(response => this.setState({ albums: response.data }));
     }
     render() {
-        return(
+        return (
             <View>
                 <Text>Projects</Text>
             </View>
